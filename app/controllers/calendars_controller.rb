9feb5177fab198d046eaa90aca9e2c8d14d74ffb
nozakikyouta <tuebkbxhmb@icloud.com>
2020-09-10 11:30:01 +0900

    Issu4修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,14 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = #Date.today.wdayを利用して添字となる数値を得る
-      #もしもwday_numが7以上であれば、7を引く
-      if #条件式を記述
-        wday_num = wday_num - 7
-      end
       days = { month: (@todays_date + x).month, date:(@todays_date+x).day, plans: today_plans, wday: wdaysから値を取り出す記述から値を取り出す記述}
-
       @week_days.push(days)
     end
 
